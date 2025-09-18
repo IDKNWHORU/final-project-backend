ef8e3e32e229b9005599d1fbf3f268c4a9ce5c5f
junho <junho9341@gmail.com>
2025-09-18 19:19:47 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# 데이터베이스 마이그레이션을 적용합니다.
-python manage.py migrate
+# 서버에서 생성된 가상 환경을 활성화합니다.
+source /home/site/wwwroot/antenv/bin/activate
 
-# 정적 파일들을 한 곳으로 모읍니다.
+# 데이터베이스 마이그레이션 및 정적 파일 수집
+python manage.py migrate
 python manage.py collectstatic --noinput
 
-# ASGI 애플리케이션을 Uvicorn 워커를 사용하여 Gunicorn으로 실행합니다.
+# [새로 추가된 라인] 모든 API 엔드포인트를 로그에 출력합니다.
+python manage.py show_my_urls
+
+# ASGI 애플리케이션 실행
 gunicorn config.asgi:application -k uvicorn.workers.UvicornWorker --chdir /home/site/wwwroot
